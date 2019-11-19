c86dd77f820d780eef344cb3f45815ea1a70d9a5
Sabina <sabina.sadowska@gmail.com>
2019-11-19 22:56:07 +0100

    snake with some issues

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
-
+var foodMove;
 
 const unit = 20;
 
-//snake
+// real random food
+var random = {
+	x: Math.floor(Math.random() * 30) * unit, 
+	y: Math.floor(Math.random() * 30) * unit,
+};
 
+function randomFood() {
+ctx.fillStyle = "rgb(253, 253, 253)"
+return ctx.fillRect(random.x, random.y, unit, unit);
+};
+
+
+
+//snake
 
 var snakeCoordinates = {
 	x: 4 * unit,
@@ -13,6 +25,7 @@ var snakeCoordinates = {
 };
 var firstSnake = true;
 if (firstSnake) {
+	ctx.fillStyle = "rgb(61, 141, 50)";
 	ctx.fillRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit)
 };
 
@@ -26,47 +39,112 @@ function snakeMove(event) {
 	var x = event.keyCode;
 	
 	if (x === 37) {
-		snakeCoordinates.x -= unit;	
-		if (randomFood.x === snakeCoordinates.x && randomFood.y === snakeCoordinates.y){
+		snakeCoordinates.x -= unit;
+		
+		if (foodX === snakeCoordinates.x && foodY === snakeCoordinates.y){
+			//randomFoodInSnake(randomFood.x, randomFood.y);
 			addFoodX();
 		}
+		
 	} else if (x === 38) {
 		snakeCoordinates.y -= unit;	
+		if (foodX === snakeCoordinates.x && foodY === snakeCoordinates.y){
+			//randomFoodInSnake(randomFood.x, randomFood.y);
+			
+			addFoodY();
+			
+		}
 		
 	} else if (x === 39) {
 		snakeCoordinates.x += unit;
+		if (foodX === snakeCoordinates.x && foodY === snakeCoordinates.y){
+			//randomFoodInSnake(randomFood.x, randomFood.y);
+			addFoodXright();
+
+			
+			
+		}
 	} else if (x === 40) {
 		snakeCoordinates.y += unit;	
+		if (foodX === snakeCoordinates.x && foodY === snakeCoordinates.y){
+			//randomFoodInSnake(randomFood.x, randomFood.y);
+			addFoodYdown();
 	}
+	};
+		
 	ctx.fillStyle = "rgb(61, 141, 50)";
+	
+
 	return ctx.fillRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit);
 };
 
 
 
-
-//food
+//food in snake
 var randomFood = {
 	x: Math.floor(Math.random() * 30) * unit, 
 	y: Math.floor(Math.random() * 30) * unit,
 };
 
+var foodX = randomFood.x;
+var foodY = randomFood.y;
+
+
+var randomFoodInSnake = function(x, y) {
+
 ctx.fillStyle = "rgb(253, 253, 253)"
-ctx.fillRect(randomFood.x, randomFood.y, unit, unit);
-	
-function addFoodX() {
-	//if (randomFood.x === snakeCoordinates.x && randomFood.y === snakeCoordinates.y) {
-	ctx.clearRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit);
-	ctx.fillRect(snakeCoordinates.x, snakeCoordinates.y, unit*2, unit);
-	};
-
-
-function addFoodY() {
-	if (randomFood.x === snakeCoordinates.x && randomFood.y === snakeCoordinates.y) {
-	ctx.clearRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit);
-	return ctx.fillRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit*2);
-	};
+return ctx.fillRect(x, y, unit, unit);
 };
+
+randomFoodInSnake(foodX, foodY);
+
+
+
+
+
+
+
+
+function addFoodX() {
+	
+	//if (randomFood.x === snakeCoordinates.x && randomFood.y === snakeCoordinates.y) {
+	ctx.clearRect(foodX, foodY, unit, unit);
+	//ctx.clearRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit);
+	snakeCoordinates.x === foodX;
+	foodX -= unit;
+	ctx.fillStyle = "rgb(61, 141, 50)"
+	ctx.fillRect(foodX, foodY, unit, unit);
+	console.log("food", randomFood, foodX);
+	console.log("snake", snakeCoordinates);
+	};
+
+
+function addFoodXright() {
+	
+	//if (randomFood.x === snakeCoordinates.x && randomFood.y === snakeCoordinates.y) {
+	ctx.clearRect(foodX, foodY, unit, unit);
+	//ctx.clearRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit);
+	snakeCoordinates.x === foodX;
+	foodX += unit;
+	ctx.fillStyle = "rgb(61, 141, 50)"
+	ctx.fillRect(foodX, foodY, unit, unit);
+	console.log("food", randomFood, foodX);
+	console.log("snake", snakeCoordinates);
+	};
+
+
+
+function addFoodYdown() {
+	ctx.clearRect(foodX, foodY, unit, unit);
+	//ctx.clearRect(snakeCoordinates.x, snakeCoordinates.y, unit, unit);
+	snakeCoordinates.y === foodY;
+	foodY += unit;
+	ctx.fillStyle = "rgb(61, 141, 50)"
+	ctx.fillRect(foodX, foodY, unit, unit);
+	console.log("food", randomFood, foodX);
+	console.log("snake", snakeCoordinates);
+	};
+
 
 
 
